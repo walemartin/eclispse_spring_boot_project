e8327477ded5dd85569d0d410542da0cc51d86e0
Olasupo Olawale Martin <rytesoft@hotmail.com>
2023-06-28 23:08:00 +0200

    3 pom

--- src/main/java/com/onion/web/controller/homecontroller.java
+++ src/main/java/com/onion/web/controller/homecontroller.java
@@ -1,5 +1,6 @@
 package com.onion.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,22 +14,26 @@ public class homecontroller {
 	
 	// Annotation
     @ResponseBody
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     // Method
     public String helloWorld()
     {
  
         return "Hello World!";
     }
-    
-    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    @GetMapping("/api/javainuse")
+	public String sayHello() {
+		return "Swagger Hello World";
+	}
+    @GetMapping("/employee")
 	public Employee firstPage() {
 
 		Employee emp = new Employee();
-		emp.setName("emp1");
-		emp.setDesignation("manager");
+		emp.setName("Olawale Olasupo");
+		emp.setDesignation("Manager");
 		emp.setId(1);
-		emp.setSalary(3000);
+		emp.setSalary(20000);
+		emp.setDept("Human Resource");
 
 		return emp;
 	}
